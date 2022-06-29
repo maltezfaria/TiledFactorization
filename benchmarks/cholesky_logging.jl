7ef6f4213f26ee240f28bf10ad6f99c73c00df35
Luiz M. Faria <maltezfaria@gmail.com>
2022-06-29 13:57:31 +0200

    update logging script for `chol`

--- benchmarks/cholesky_logging.jl
+++ benchmarks/cholesky_logging.jl
@@ -10,6 +10,8 @@ capacity = 100
 sch = DataFlowTasks.JuliaScheduler(capacity)
 DataFlowTasks.setscheduler!(sch)
 
+cp = Val(true)
+
 m         = 5_000
 tile_size = 256
 # create an SPD matrix
@@ -17,16 +19,18 @@ A = TF.spd_matrix(m)
 nt = Threads.nthreads()
 # go once to compile
 B = copy(A)
-F = TF.cholesky!(B,tile_size)
+F = TF.cholesky!(B,tile_size;copy=cp)
+@info "error: $(norm(F.L*F.U-A,Inf))"
 GC.gc()
-te = @elapsed TF.cholesky!(B,tile_size)
+te = @elapsed TF.cholesky!(B,tile_size;copy=cp)
 @info "elapsed: $te"
+
 GC.gc()
 # reset logger and run it again
 logger = DataFlowTasks.getlogger()
 DataFlowTasks.resetcounter!()
 DataFlowTasks.resetlogger!()
-F = TF.cholesky!(B,tile_size)
+F = TF.cholesky!(B,tile_size;copy=cp)
 @info "Number of tasks = $(DataFlowTasks.TASKCOUNTER[])"
 @info "Number of threads = $(Threads.nthreads())"
 plot(logger;categories=["chol","ldiv","schur","copyin","copyout","Cholesky"])
